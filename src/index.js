06ef4a50c34510bf7deafb7b8cf64d262129638e
neeteshjnvrec-cloud <neeteshjnvrec@gmail.com>
2026-01-27 05:07:27 +0000

    Restore source code with correct configuration

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import "@/index.css";
 import App from "@/App";
 
+// Handle redirect from 404.html
+if (window.location.search.includes('redirect=')) {
+  const redirectPath = decodeURIComponent(window.location.search.split('redirect=')[1]);
+  window.history.replaceState(null, null, redirectPath);
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
